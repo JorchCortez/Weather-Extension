a63357a4b71c6542ac7b6c8b7cb0b28798ef0873
Joorch <jacortez_94@hotmail.com>
2021-12-07 17:40:52 -0600

    adding base functionality for storing favorite places

--- popup.js
+++ popup.js
@@ -3,8 +3,20 @@ const api = {
     baseurl:"https://api.openweathermap.org/data/2.5/",
 }
 
+//Search box event listener
 const searchBox = document.querySelector('.search-box');
 searchBox.addEventListener('keypress', setQuery);
+
+//Save city click event listener 
+const pin = document.querySelector('.icon');
+pin.addEventListener('click', pinAddress);
+ 
+
+function pinAddress(){
+    let city = document.querySelector('.location .city').innerText;
+    console.log(city)
+    saveCities(city)
+}
 
 function setQuery(evt){
     if(evt.keyCode==13){
@@ -12,7 +24,8 @@ function setQuery(evt){
     }
 }
 
-const getResults = (query) => {
+function getResults(query) {
+    console.log(query)
     fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
         return weather.json();
@@ -24,7 +37,7 @@ const displayResults = (weather) =>{
     
     //Setting the city, Country
     let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
+    city.innerHTML = `<span class="icon"><i class="fas fa-thumbtack"></i></span>${weather.name}, ${weather.sys.country}`;
 
     //Setting up the date
     let now = new Date();
@@ -44,8 +57,6 @@ const displayResults = (weather) =>{
     hilo.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C `
 }
 
-
-
 const dateBuilder = (d) => {
     let months = [  'January', 'February', 'March', 
                     'April', 'May', 'June', 
@@ -61,3 +72,29 @@ const dateBuilder = (d) => {
     let year = d.getFullYear();
     return `${day} ${date} ${month} ${year}` 
 }
+
+const saveCities = (city) => {
+
+    /*let cities = chrome.storage.sync.get(["currentCities"], function(items){
+        //  items = [ { "yourBody": "myBody" } ]
+    });
+    console.log(cities)
+    */
+    //chrome.storage.sync.set({ "currentCities": city }, function(){});  
+    loadSavedCities(currSaved); 
+}
+
+const loadSavedCities = (c) =>{
+    let places = document.querySelector('.saved-places')
+    let savedPlaces = ""
+    c.forEach(el => {
+        savedPlaces += `<div class="place" onclick="getResults("${el}")" >${el}</div>`
+    });
+    places.innerHTML = savedPlaces;
+}
+
+
+let currSaved = [
+    "Monterrey, MX",
+    "Vancouver, CA"
+    ]
